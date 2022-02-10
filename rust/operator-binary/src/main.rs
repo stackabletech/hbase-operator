cee581ac980f3618a8117dea817b034dcf260258
Siegfried Weber <mail@siegfriedweber.net>
2022-02-10 16:00:40 +0100

    Remove unnecessary function erase_controller_result_type

--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -7,13 +7,9 @@ use stackable_operator::{
     cli::{Command, ProductOperatorRun},
     k8s_openapi::api::{apps::v1::StatefulSet, core::v1::Service},
     kube::{
-        api::{DynamicObject, ListParams},
-        runtime::{
-            controller::{Context, ReconcilerAction},
-            reflector::ObjectRef,
-            Controller,
-        },
-        CustomResourceExt, Resource,
+        api::ListParams,
+        runtime::{controller::Context, Controller},
+        CustomResourceExt,
     },
 };
 
@@ -26,17 +22,6 @@ mod built_info {
 struct Opts {
     #[clap(subcommand)]
     cmd: Command,
-}
-
-/// Erases the concrete types of the controller result, so that we can merge the streams of multiple controllers for different resources.
-///
-/// In particular, we convert `ObjectRef<K>` into `ObjectRef<DynamicObject>` (which carries `K`'s metadata at runtime instead), and
-/// `E` into the trait object `anyhow::Error`.
-fn erase_controller_result_type<K: Resource, E: std::error::Error + Send + Sync + 'static>(
-    res: Result<(ObjectRef<K>, ReconcilerAction), E>,
-) -> anyhow::Result<(ObjectRef<DynamicObject>, ReconcilerAction)> {
-    let (obj_ref, action) = res?;
-    Ok((obj_ref.erase(), action))
 }
 
 #[tokio::main]
@@ -62,28 +47,25 @@ async fn main() -> anyhow::Result<()> {
             let client =
                 stackable_operator::client::create_client(Some("hbase.stackable.tech".to_string()))
                     .await?;
-            let hbase_controller =
-                Controller::new(client.get_all_api::<HbaseCluster>(), ListParams::default())
-                    .owns(client.get_all_api::<Service>(), ListParams::default())
-                    .owns(client.get_all_api::<StatefulSet>(), ListParams::default())
-                    .shutdown_on_signal()
-                    .run(
-                        hbase_controller::reconcile_hbase,
-                        hbase_controller::error_policy,
-                        Context::new(hbase_controller::Ctx {
-                            client: client.clone(),
-                            product_config,
-                        }),
-                    );
 
-            hbase_controller
-                .map(erase_controller_result_type)
+            Controller::new(client.get_all_api::<HbaseCluster>(), ListParams::default())
+                .owns(client.get_all_api::<Service>(), ListParams::default())
+                .owns(client.get_all_api::<StatefulSet>(), ListParams::default())
+                .shutdown_on_signal()
+                .run(
+                    hbase_controller::reconcile_hbase,
+                    hbase_controller::error_policy,
+                    Context::new(hbase_controller::Ctx {
+                        client: client.clone(),
+                        product_config,
+                    }),
+                )
                 .for_each(|res| async {
                     match res {
                         Ok((obj, _)) => tracing::info!(object = %obj, "Reconciled object"),
                         Err(err) => {
                             tracing::error!(
-                                error = &*err as &dyn std::error::Error,
+                                error = &err as &dyn std::error::Error,
                                 "Failed to reconcile object",
                             )
                         }
