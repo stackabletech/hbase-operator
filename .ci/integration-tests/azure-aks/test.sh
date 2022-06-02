2dceabb651068347343a769c59f8fc77bcc97d79
Siegfried Weber <mail@siegfriedweber.net>
2022-06-02 11:45:56 +0200

    Remove HBase 2.4.6 and ZooKeeper 3.5.8 from the integration tests and run four tests in parallel

--- .ci/integration-tests/azure-aks/test.sh
+++ .ci/integration-tests/azure-aks/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone -b "$GIT_BRANCH" https://github.com/stackabletech/hbase-operator.git
-(cd hbase-operator/ && ./scripts/run_tests.sh --parallel 2)
+(cd hbase-operator/ && ./scripts/run_tests.sh --parallel 4)
 exit_code=$?
 ./operator-logs.sh hbase > /target/hbase-operator.log
 ./operator-logs.sh hdfs > /target/hdfs-operator.log
