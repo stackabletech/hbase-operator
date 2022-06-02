2ecf3d3749e3ce852d9e61b881075212c8b75fc0
Siegfried Weber <mail@siegfriedweber.net>
2022-06-02 09:43:28 +0200

    Run only two integration tests in parallel

--- .ci/integration-tests/aws-eks/test.sh
+++ .ci/integration-tests/aws-eks/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone -b "$GIT_BRANCH" https://github.com/stackabletech/hbase-operator.git
-(cd hbase-operator/ && ./scripts/run_tests.sh)
+(cd hbase-operator/ && ./scripts/run_tests.sh --parallel 2)
 exit_code=$?
 ./operator-logs.sh hbase > /target/hbase-operator.log
 ./operator-logs.sh hdfs > /target/hdfs-operator.log
