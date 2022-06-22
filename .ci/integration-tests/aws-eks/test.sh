96ee9e4e76af57d3fdf5681c50b26e388ef23195
Nikolaus Winter <mail@nikolaus-winter.de>
2022-06-22 15:00:02 +0000

    removed CI files (#194)
    # Description
    
    Removed CI files

--- .ci/integration-tests/aws-eks/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git clone -b "$GIT_BRANCH" https://github.com/stackabletech/hbase-operator.git
-(cd hbase-operator/ && ./scripts/run_tests.sh --parallel 4)
-exit_code=$?
-./operator-logs.sh hbase > /target/hbase-operator.log
-./operator-logs.sh hdfs > /target/hdfs-operator.log
-./operator-logs.sh zookeeper > /target/zookeeper-operator.log
-exit $exit_code
