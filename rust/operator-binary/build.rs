bc6348dc26dd8da8e7dfeee8d88b72544c3b30cc
Malte Sander <malte.sander.it@gmail.com>
2023-01-10 11:50:07 +0100

    removed deploy crd folder

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -1,8 +1,3 @@
-use stackable_hbase_crd::HbaseCluster;
-use stackable_operator::crd::CustomResourceExt;
-
 fn main() {
     built::write_built_file().expect("Failed to acquire build-time information");
-
-    HbaseCluster::write_yaml_schema("../../deploy/crd/hbasecluster.crd.yaml").unwrap();
 }
