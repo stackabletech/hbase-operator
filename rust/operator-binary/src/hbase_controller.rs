ddb289ad87cb209730a606c4c6c0aa96b51d2f81
Malte Sander <malte.sander.it@gmail.com>
2023-10-17 14:45:29 +0200

    bump operator-rs to 0.55.0

--- rust/operator-binary/src/hbase_controller.rs
+++ rust/operator-binary/src/hbase_controller.rs
@@ -27,7 +27,6 @@ use stackable_operator::{
         product_image_selection::ResolvedProductImage,
         rbac::{build_rbac_resources, service_account_name},
     },
-    duration::Duration,
     k8s_openapi::{api::core::v1::Volume, DeepMerge},
     k8s_openapi::{
         api::{
@@ -61,6 +60,7 @@ use stackable_operator::{
         compute_conditions, operations::ClusterOperationsConditionBuilder,
         statefulset::StatefulSetConditionBuilder,
     },
+    time::Duration,
 };
 use std::{
     collections::{BTreeMap, HashMap},
