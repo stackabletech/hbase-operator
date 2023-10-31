4eaa96dcd950d4d15fde7aff01137990b865ed61
Techassi <sascha.lautenschlaeger@stackable.tech>
2023-10-31 15:05:15 +0100

    Use updated product-config (#408)

--- rust/operator-binary/src/hbase_controller.rs
+++ rust/operator-binary/src/hbase_controller.rs
@@ -1,15 +1,15 @@
 //! Ensures that `Pod`s are configured and running for each [`HbaseCluster`]
-
-use crate::{
-    discovery::build_discovery_configmap,
-    operations::pdb::add_pdbs,
-    product_logging::{
-        extend_role_group_config_map, resolve_vector_aggregator_address, LOG4J_CONFIG_FILE,
-    },
-    zookeeper::{self, ZookeeperConnectionInformation},
-    OPERATOR_NAME,
+use std::{
+    collections::{BTreeMap, HashMap},
+    str::FromStr,
+    sync::Arc,
 };
 
+use product_config::{
+    types::PropertyNameKind,
+    writer::{to_hadoop_xml, to_java_properties_string, PropertiesWriterError},
+    ProductConfigManager,
+};
 use snafu::{OptionExt, ResultExt, Snafu};
 use stackable_hbase_crd::{
     Container, HbaseCluster, HbaseClusterStatus, HbaseConfig, HbaseConfigFragment, HbaseRole,
@@ -27,7 +27,6 @@ use stackable_operator::{
         product_image_selection::ResolvedProductImage,
         rbac::{build_rbac_resources, service_account_name},
     },
-    duration::Duration,
     k8s_openapi::{api::core::v1::Volume, DeepMerge},
     k8s_openapi::{
         api::{
@@ -43,11 +42,6 @@ use stackable_operator::{
     labels::{role_group_selector_labels, role_selector_labels, ObjectLabels},
     logging::controller::ReconcilerError,
     memory::{BinaryMultiple, MemoryQuantity},
-    product_config::{
-        types::PropertyNameKind,
-        writer::{self, to_java_properties_string},
-        ProductConfigManager,
-    },
     product_config_utils::{transform_all_roles_to_config, validate_all_roles_and_groups_config},
     product_logging::{
         self,
@@ -61,13 +55,19 @@ use stackable_operator::{
         compute_conditions, operations::ClusterOperationsConditionBuilder,
         statefulset::StatefulSetConditionBuilder,
     },
-};
-use std::{
-    collections::{BTreeMap, HashMap},
-    str::FromStr,
-    sync::Arc,
+    time::Duration,
 };
 use strum::{EnumDiscriminants, IntoStaticStr};
+
+use crate::{
+    discovery::build_discovery_configmap,
+    operations::pdb::add_pdbs,
+    product_logging::{
+        extend_role_group_config_map, resolve_vector_aggregator_address, LOG4J_CONFIG_FILE,
+    },
+    zookeeper::{self, ZookeeperConnectionInformation},
+    OPERATOR_NAME,
+};
 
 pub const HBASE_CONTROLLER_NAME: &str = "hbasecluster";
 pub const STACKABLE_LOG_DIR: &str = "/stackable/log";
@@ -212,7 +212,7 @@ pub enum Error {
         rolegroup
     ))]
     SerializeJvmSecurity {
-        source: stackable_operator::product_config::writer::PropertiesWriterError,
+        source: PropertiesWriterError,
         rolegroup: RoleGroupRef<HbaseCluster>,
     },
     #[snafu(display("failed to create PodDisruptionBudget"))]
@@ -507,7 +507,7 @@ fn build_rolegroup_config_map(
         )
         .add_data(
             HBASE_SITE_XML,
-            writer::to_hadoop_xml(
+            to_hadoop_xml(
                 hbase_site_config
                     .into_iter()
                     .map(|(k, v)| (k, Some(v)))
