87c55e58e88c17979037ecc1944ef0fa12db8bd7
Nick Larsen <nick.larsen@stackable.tech>
2024-01-11 14:13:28 +0100

    remove unused imports from legacy roleGroup node selector removal

--- rust/crd/src/affinity.rs
+++ rust/crd/src/affinity.rs
@@ -84,11 +84,10 @@ mod tests {
 
     use crate::HbaseCluster;
     use stackable_operator::{
-        commons::affinity::{StackableAffinity, StackableNodeSelector},
+        commons::affinity::StackableAffinity,
         k8s_openapi::{
             api::core::v1::{
-                NodeAffinity, NodeSelector, NodeSelectorRequirement, NodeSelectorTerm, PodAffinity,
-                PodAffinityTerm, PodAntiAffinity, WeightedPodAffinityTerm,
+                PodAffinity, PodAffinityTerm, PodAntiAffinity, WeightedPodAffinityTerm,
             },
             apimachinery::pkg::apis::meta::v1::LabelSelector,
         },
