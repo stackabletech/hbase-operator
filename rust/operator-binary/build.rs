acc553dd741f40ef1cb1c10e5e5b124c26a0de9b
Sönke Liebau <soenke.liebau@stackable.de>
2024-02-12 16:00:50 +0100

    Revert "Update built dependency to 0.7.1 (#452)" (#453)
    This reverts commit f6c1fbfaa433f10d932f9bf02e845a62b24f2f77.

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -5,7 +5,8 @@ fn main() {
     built::write_built_file_with_opts(
         // built's env module depends on a whole bunch of variables that crate2nix doesn't provide
         // so we grab the specific env variables that we care about out ourselves instead.
-        Some("Cargo.toml".as_ref()),
+        built::Options::default().set_env(false),
+        "Cargo.toml".as_ref(),
         &out_dir.join("built.rs"),
     )
     .unwrap();
