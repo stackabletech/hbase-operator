a736dc6ed5d4f41bdbffc32425e5c6e18ac40f83
Siegfried Weber <mail@siegfriedweber.net>
2024-04-18 12:14:33 +0200

    Test snapshot export to s3 (#486)
    * Create integration test for exporting a snapshot to S3
    
    * Use script export-snapshot-to-s3 in the integration test
    
    * Fix shellcheck warning
    
    * Rename test files
    
    * Add assertions to the snapshot-export test
    
    * Use the correct HBase image in the test Job

---
+++ tests/templates/kuttl/snapshot-export/30_test-export.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+export \
+    AWS_ACCESS_KEY_ID=hbaseAccessKey \
+    AWS_SECRET_KEY=hbaseSecretKey \
+    AWS_ENDPOINT=http://minio:9000/ \
+    AWS_SSL_ENABLED=false \
+    AWS_PATH_STYLE_ACCESS=true
+
+# Create local snapshot
+hbase shell create-snapshot.hbase | \
+    tee /dev/stderr | \
+    grep '=> \["snap"\]' > /dev/null
+
+# Export local snapshot to S3
+export-snapshot-to-s3 \
+        --snapshot snap \
+        --copy-to s3a://hbase/snap \
+        --overwrite | \
+    tee /dev/stderr | \
+    grep 'Export Completed: snap' > /dev/null
+
+# Delete local snapshot
+hbase shell delete-snapshot.hbase | \
+    tee /dev/stderr | \
+    grep '=> \[\]' > /dev/null
+
+# Import snapshot from S3
+export-snapshot-to-s3 \
+        --snapshot snap \
+        --copy-from s3a://hbase/snap \
+        --copy-to hdfs://test-hdfs/hbase \
+        --overwrite | \
+    tee /dev/stderr | \
+    grep 'Export Completed: snap' > /dev/null
+
+# Restore imported snapshot
+hbase shell restore-snapshot.hbase | \
+    tee /dev/stderr | \
+    grep 'value=42' > /dev/null
