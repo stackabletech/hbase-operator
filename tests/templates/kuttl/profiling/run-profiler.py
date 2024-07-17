119ccef502f879fdfd09486c316c5006f26523c3
Nick Larsen <nick.larsen@stackable.tech>
2024-07-17 17:12:36 +0200

    chore(pre-commit): format python with ruff

--- tests/templates/kuttl/profiling/run-profiler.py
+++ tests/templates/kuttl/profiling/run-profiler.py
@@ -9,30 +9,33 @@ PROFILING_DURATION_IN_SEC = 1
 def start_profiling_and_get_refresh_header(service_url):
     prof_page = requests.get(
         f"{service_url}/prof"
-        f"?event={EVENT_TYPE}&duration={PROFILING_DURATION_IN_SEC}")
+        f"?event={EVENT_TYPE}&duration={PROFILING_DURATION_IN_SEC}"
+    )
 
-    assert prof_page.ok, \
-        f"""Profiling could not be started.
+    assert prof_page.ok, f"""Profiling could not be started.
         URL: {prof_page.request.url}
         Status Code: {prof_page.status_code}"""
 
-    return prof_page.headers['Refresh']
+    return prof_page.headers["Refresh"]
 
 
 def parse_refresh_header(refresh_header):
-    refresh_time_in_sec, refresh_path = refresh_header.split(';', 1)
+    refresh_time_in_sec, refresh_path = refresh_header.split(";", 1)
     refresh_time_in_sec = int(refresh_time_in_sec)
 
-    assert refresh_time_in_sec == PROFILING_DURATION_IN_SEC, \
-        f"""Profiling duration and refresh time should be equal.
+    assert (
+        refresh_time_in_sec == PROFILING_DURATION_IN_SEC
+    ), f"""Profiling duration and refresh time should be equal.
         expected: {PROFILING_DURATION_IN_SEC}
         actual:   {refresh_time_in_sec}"""
 
     # HBase 2.6 changed the output of the perf call to end with cpu-\d+.html
-    expected_refresh_path_pattern = \
-        r'/prof-output-hbase/async-prof-pid-\d+-(itimer-\d+.flamegraph|cpu-\d+.html)'
-    assert re.fullmatch(expected_refresh_path_pattern, refresh_path), \
-        f"""The path to the flamegraph contains an unexpected pattern.
+    expected_refresh_path_pattern = (
+        r"/prof-output-hbase/async-prof-pid-\d+-(itimer-\d+.flamegraph|cpu-\d+.html)"
+    )
+    assert re.fullmatch(
+        expected_refresh_path_pattern, refresh_path
+    ), f"""The path to the flamegraph contains an unexpected pattern.
         expected pattern: {expected_refresh_path_pattern}"
         actual path:      {refresh_path}"""
 
@@ -47,23 +50,21 @@ def wait_for_profiling_to_finish(refresh_time_in_sec):
 def fetch_flamegraph(service_url, refresh_path):
     flamegraph_page = requests.get(f"{service_url}{refresh_path}")
 
-    assert flamegraph_page.ok, \
-        f"""The flamegraph could not be fetched.
+    assert flamegraph_page.ok, f"""The flamegraph could not be fetched.
         URL: {flamegraph_page.request.url}
         Status Code: {flamegraph_page.status_code}"""
 
 
 def test_profiling(role, port):
     service_url = (
-        f"http://test-hbase-{role}-default-0.test-hbase-{role}-default"
-        f":{port}")
+        f"http://test-hbase-{role}-default-0.test-hbase-{role}-default" f":{port}"
+    )
 
     print(f"Test profiling on {service_url}")
 
     refresh_header = start_profiling_and_get_refresh_header(service_url)
 
-    refresh_time_in_sec, refresh_path = \
-        parse_refresh_header(refresh_header)
+    refresh_time_in_sec, refresh_path = parse_refresh_header(refresh_header)
 
     wait_for_profiling_to_finish(refresh_time_in_sec)
 
