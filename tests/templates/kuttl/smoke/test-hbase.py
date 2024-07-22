b659f45cb35bf56f1bd4940334da6e80d46b0a4e
Nick <10092581+NickLarsenNZ@users.noreply.github.com>
2024-07-22 09:08:56 +0200

    docs(getting-started): update stackablectl op install output (#528)
    * docs(getting-started): update stackablectl op install output
    
    * chore(getting-started): improve the getting_started.sh script (template)
    
    * fix(getting-started): phoenix introduces a new table we need to check for
    
    * chore(pre-commit): add trailing new line
    
    * chore(pre-commit): format python with ruff
    
    * chore(getting-started): parallalise the helm installs
    
    ---------
    
    Co-authored-by: Nick <NickLarsenNZ@users.noreply.github.com>

--- tests/templates/kuttl/smoke/test-hbase.py
+++ tests/templates/kuttl/smoke/test-hbase.py
@@ -8,32 +8,34 @@ import sys
 class HbaseClient:
     def __init__(self):
         self.session = requests.Session()
-        self.session.headers.update({
-            'Accept': 'text/xml',
-            'Content-Type': 'text/xml',
-        })
+        self.session.headers.update(
+            {
+                "Accept": "text/xml",
+                "Content-Type": "text/xml",
+            }
+        )
         http.client.HTTPConnection.debuglevel = 1
 
     @staticmethod
     def encode_value(plain_string):
-        return base64.b64encode(plain_string.encode('utf-8')).decode('ascii')
+        return base64.b64encode(plain_string.encode("utf-8")).decode("ascii")
 
     @staticmethod
     def decode_value(base64_string):
-        return base64.b64decode(base64_string).decode('utf-8')
+        return base64.b64decode(base64_string).decode("utf-8")
 
     def create_table(self, rest_url, name, column_family, compression):
         response = self.session.put(
-            f'{rest_url}/{name}/schema',
-            data=f'''
+            f"{rest_url}/{name}/schema",
+            data=f"""
                     <TableSchema name="{name}">
                         <ColumnSchema name="{column_family}" COMPRESSION="{compression}" />
                     </TableSchema>
-                '''
+                """,
         )
         assert response.status_code == 201
-        table_schema_location = response.headers['location']
-        table_location = table_schema_location.removesuffix('/schema')
+        table_schema_location = response.headers["location"]
+        table_location = table_schema_location.removesuffix("/schema")
         return (table_location, table_schema_location)
 
     def delete_table(self, table_schema_location):
@@ -41,10 +43,10 @@ class HbaseClient:
         assert response.status_code == 200
 
     def put_row(self, table_location, row_key, column_family, column, cell_value):
-        cell_column = f'{column_family}:{column}'
+        cell_column = f"{column_family}:{column}"
         response = self.session.put(
-            f'{table_location}/{row_key}',
-            data=f'''
+            f"{table_location}/{row_key}",
+            data=f"""
                     <CellSet>
                         <Row key="{self.encode_value(row_key)}">
                             <Cell column="{self.encode_value(cell_column)}">
@@ -52,17 +54,16 @@ class HbaseClient:
                             </Cell>
                         </Row>
                     </CellSet>
-                '''
+                """,
         )
         assert response.status_code == 200
 
     def put_scanner(self, table_location):
         response = self.session.put(
-            f'{table_location}/scanner',
-            data='<Scanner batch="1" />'
+            f"{table_location}/scanner", data='<Scanner batch="1" />'
         )
         assert response.status_code == 201
-        return response.headers['location']
+        return response.headers["location"]
 
     def get_scanner(self, scanner_location):
         response = self.session.get(scanner_location)
@@ -79,56 +80,56 @@ hbase_rest_url = sys.argv[1]
 hbase = HbaseClient()
 
 # supported compression algorithms: SNAPPY, GZ
-compression_opts = ['NONE', 'SNAPPY', 'GZ']
+compression_opts = ["NONE", "SNAPPY", "GZ"]
 
 for compression in compression_opts:
-    print(f'''
+    print(f"""
     Create a table with compression={compression}
-    ==============''')
-    column_family = 'cf'
+    ==============""")
+    column_family = "cf"
     (table_location, table_schema_location) = hbase.create_table(
         rest_url=hbase_rest_url,
-        name='companies',
+        name="companies",
         column_family=column_family,
-        compression=compression
+        compression=compression,
     )
 
-    print('''
+    print("""
     Write a row to the table
-    ========================''')
-    cell_value = 'Stackable GmbH'
+    ========================""")
+    cell_value = "Stackable GmbH"
     hbase.put_row(
         table_location=table_location,
-        row_key='stackable',
+        row_key="stackable",
         column_family=column_family,
-        column='name',
-        cell_value=cell_value
+        column="name",
+        cell_value=cell_value,
     )
 
-    print('''
+    print("""
     Get a scanner object
-    ====================''')
+    ====================""")
     scanner_location = hbase.put_scanner(table_location)
 
-    print('''
+    print("""
     Get the next batch from the scanner
-    ===================================''')
+    ===================================""")
     scan = hbase.get_scanner(scanner_location)
 
-    print('''
+    print("""
     Verify table content
-    ====================''')
+    ====================""")
     parser = ET.fromstring(scan)
-    actual_cell_value = hbase.decode_value(parser.findtext('./Row/Cell'))
+    actual_cell_value = hbase.decode_value(parser.findtext("./Row/Cell"))
     print(f'assert "{actual_cell_value}" == "{cell_value}"')
     assert actual_cell_value == cell_value
 
-    print('''
+    print("""
     Delete the scanner
-    ==================''')
+    ==================""")
     hbase.delete_scanner(scanner_location)
 
-    print('''
+    print("""
     Delete the table
-    ================''')
+    ================""")
     hbase.delete_table(table_schema_location)
