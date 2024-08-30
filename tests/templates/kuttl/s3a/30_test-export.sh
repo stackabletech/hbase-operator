217f2fe635812e46585eb595098654f2324a6a67
Siegfried Weber <mail@siegfriedweber.net>
2024-08-30 18:19:01 +0200

    Add test for s3a

---
+++ tests/templates/kuttl/s3a/30_test-export.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+export \
+    AWS_ACCESS_KEY_ID=hbaseAccessKey \
+    AWS_SECRET_KEY=hbaseSecretKey \
+    AWS_ENDPOINT=http://minio:9000/ \
+    AWS_SSL_ENABLED=false \
+    AWS_PATH_STYLE_ACCESS=true
+
+# Create local snapshot
+hbase shell create-snapshot.hbase 2>&1 | \
+    grep '=> \["snap"\]' > /dev/null
+
+# Export local snapshot to S3
+export-snapshot-to-s3 \
+        --snapshot snap \
+        --copy-to s3a://hbase/snap \
+        --overwrite 2>&1 | \
+    grep 'Export Completed: snap' > /dev/null
+
+# Delete local snapshot
+hbase shell delete-snapshot.hbase 2>&1 | \
+    grep '=> \[\]' > /dev/null
+
+# Import snapshot from S3
+export-snapshot-to-s3 \
+        --snapshot snap \
+        --copy-from s3a://hbase/snap \
+        --copy-to s3a://hbase-root/ \
+        --overwrite 2>&1 | \
+    grep 'Export Completed: snap' > /dev/null
+
+# Restore imported snapshot
+hbase shell restore-snapshot.hbase 2>&1 | \
+    grep 'value=42' > /dev/null
