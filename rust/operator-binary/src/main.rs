842a123d9fa840a1eb23b406b71a7060f4d350c1
Techassi <git@techassi.dev>
2025-04-04 13:31:24 +0200

    feat!: Replace log initialization with stackable-telemetry
    BREAKING: The log directory variable for the rolling file appender has changed

--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -1,11 +1,11 @@
-use std::sync::Arc;
+use std::{ops::Deref as _, sync::Arc};
 
 use clap::Parser;
 use futures::StreamExt;
 use hbase_controller::FULL_HBASE_CONTROLLER_NAME;
 use stackable_operator::{
     YamlSchema,
-    cli::{Command, ProductOperatorRun},
+    cli::{Command, ProductOperatorRun, RollingPeriod},
     k8s_openapi::api::{apps::v1::StatefulSet, core::v1::Service},
     kube::{
         core::DeserializeGuard,
@@ -18,8 +18,10 @@ use stackable_operator::{
     logging::controller::report_controller_reconciled,
     shared::yaml::SerializeOptions,
 };
+use stackable_telemetry::{Tracing, tracing::settings::Settings};
+use tracing::level_filters::LevelFilter;
 
-use crate::crd::{APP_NAME, HbaseCluster, v1alpha1};
+use crate::crd::{HbaseCluster, v1alpha1};
 
 mod config;
 mod crd;
@@ -55,14 +57,50 @@ async fn main() -> anyhow::Result<()> {
         Command::Run(ProductOperatorRun {
             product_config,
             watch_namespace,
-            tracing_target,
+            telemetry_arguments,
             cluster_info_opts,
         }) => {
-            stackable_operator::logging::initialize_logging(
-                "HBASE_OPERATOR_LOG",
-                APP_NAME,
-                tracing_target,
-            );
+            let _tracing_guard = Tracing::builder()
+                // TODO (@Techassi): This should be a constant
+                .service_name("hbase-operator")
+                .with_console_output((
+                    // TODO (@Techassi): Change to CONSOLE_LOG, create constant
+                    "HBASE_OPERATOR_LOG",
+                    LevelFilter::INFO,
+                    !telemetry_arguments.no_console_output,
+                ))
+                // NOTE (@Techassi): Before stackable-telemetry was used, the log directory was
+                // set via an env: `HBASE_OPERATOR_LOG_DIRECTORY`.
+                // See: https://github.com/stackabletech/operator-rs/blob/f035997fca85a54238c8de895389cc50b4d421e2/crates/stackable-operator/src/logging/mod.rs#L40
+                // Now it will be `ROLLING_LOGS` (or via `--rolling-logs <DIRECTORY>`).
+                .with_file_output(telemetry_arguments.rolling_logs.map(|log_directory| {
+                    let rotation_period = telemetry_arguments
+                        .rolling_logs_period
+                        .unwrap_or(RollingPeriod::Hourly)
+                        .deref()
+                        .clone();
+
+                    Settings::builder()
+                        // TODO (@Techassi): Change to CONSOLE_LOG or FILE_LOG, create constant
+                        .with_environment_variable("HBASE_OPERATOR_LOG")
+                        .with_default_level(LevelFilter::INFO)
+                        .file_log_settings_builder(log_directory, "tracing-rs.log")
+                        .with_rotation_period(rotation_period)
+                        .build()
+                }))
+                .with_otlp_log_exporter((
+                    "OTLP_LOG",
+                    LevelFilter::DEBUG,
+                    telemetry_arguments.otlp_logs,
+                ))
+                .with_otlp_trace_exporter((
+                    "OTLP_TRACE",
+                    LevelFilter::DEBUG,
+                    telemetry_arguments.otlp_traces,
+                ))
+                .build()
+                .init()?;
+
             tracing::info!(
                 built_info.pkg_version = built_info::PKG_VERSION,
                 built_info.git_version = built_info::GIT_VERSION,
@@ -72,6 +110,7 @@ async fn main() -> anyhow::Result<()> {
                 "Starting {description}",
                 description = built_info::PKG_DESCRIPTION
             );
+
             let product_config = product_config.load(&[
                 "deploy/config-spec/properties.yaml",
                 "/etc/stackable/hbase-operator/config-spec/properties.yaml",
