6691bb80ec04bf8ec5032e4419fd4ebbe8cb2a96
xeniape <xenia.fischer@stackable.tech>
2025-10-06 16:37:30 +0200

    feat: Add EoS checker

--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -5,11 +5,12 @@
 use std::sync::Arc;
 
 use clap::Parser;
-use futures::StreamExt;
+use futures::{FutureExt, StreamExt};
 use hbase_controller::FULL_HBASE_CONTROLLER_NAME;
 use stackable_operator::{
     YamlSchema,
-    cli::{Command, ProductOperatorRun},
+    cli::{Command, RunArguments},
+    eos::EndOfSupportChecker,
     k8s_openapi::api::{
         apps::v1::StatefulSet,
         core::v1::{ConfigMap, Service},
@@ -62,18 +63,19 @@ async fn main() -> anyhow::Result<()> {
             HbaseCluster::merged_crd(HbaseClusterVersion::V1Alpha1)?
                 .print_yaml_schema(built_info::PKG_VERSION, SerializeOptions::default())?;
         }
-        Command::Run(ProductOperatorRun {
+        Command::Run(RunArguments {
             product_config,
             watch_namespace,
             operator_environment: _,
-            telemetry,
-            cluster_info,
+            maintenance,
+            common,
         }) => {
             // NOTE (@NickLarsenNZ): Before stackable-telemetry was used:
             // - The console log level was set by `HBASE_OPERATOR_LOG`, and is now `CONSOLE_LOG` (when using Tracing::pre_configured).
             // - The file log level was set by `HBASE_OPERATOR_LOG`, and is now set via `FILE_LOG` (when using Tracing::pre_configured).
             // - The file log directory was set by `HBASE_OPERATOR_LOG_DIRECTORY`, and is now set by `ROLLING_LOGS_DIR` (or via `--rolling-logs <DIRECTORY>`).
-            let _tracing_guard = Tracing::pre_configured(built_info::PKG_NAME, telemetry).init()?;
+            let _tracing_guard =
+                Tracing::pre_configured(built_info::PKG_NAME, common.telemetry).init()?;
 
             tracing::info!(
                 built_info.pkg_version = built_info::PKG_VERSION,
@@ -85,13 +87,18 @@ async fn main() -> anyhow::Result<()> {
                 description = built_info::PKG_DESCRIPTION
             );
 
+            let eos_checker =
+                EndOfSupportChecker::new(built_info::BUILT_TIME_UTC, maintenance.end_of_support)?
+                    .run()
+                    .map(anyhow::Ok);
+
             let product_config = product_config.load(&[
                 "deploy/config-spec/properties.yaml",
                 "/etc/stackable/hbase-operator/config-spec/properties.yaml",
             ])?;
             let client = stackable_operator::client::initialize_operator(
                 Some(OPERATOR_NAME.to_string()),
-                &cluster_info,
+                &common.cluster_info,
             )
             .await?;
 
@@ -108,7 +115,7 @@ async fn main() -> anyhow::Result<()> {
                 watcher::Config::default(),
             );
             let config_map_store = hbase_controller.store();
-            hbase_controller
+            let hbase_controller = hbase_controller
                 .owns(
                     watch_namespace.get_api::<Service>(&client),
                     watcher::Config::default(),
@@ -153,7 +160,9 @@ async fn main() -> anyhow::Result<()> {
                         }
                     },
                 )
-                .await;
+                .map(anyhow::Ok);
+
+            futures::try_join!(hbase_controller, eos_checker)?;
         }
     }
 
