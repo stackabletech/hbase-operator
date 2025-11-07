870a9241dd95a34f5b4284488654887d05f4a0fa
Techassi <git@techassi.dev>
2025-11-07 09:47:18 +0100

    chore: Release 25.11.0
    Signed-off-by: Techassi <git@techassi.dev>

--- Cargo.nix
+++ Cargo.nix
@@ -7991,7 +7991,7 @@ rec {
       };
       "stackable-hbase-operator" = rec {
         crateName = "stackable-hbase-operator";
-        version = "25.11.0-rc1";
+        version = "25.11.0";
         edition = "2021";
         crateBin = [
           {
