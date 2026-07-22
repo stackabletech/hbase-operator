3748f190520a7ef6a6246a056f216bbe78212d80
Siegfried Weber <mail@siegfriedweber.net>
2026-07-22 17:55:22 +0200

    chore: Upgrade stackable-operator to 0.114.0

--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -66,7 +66,6 @@ async fn main() -> anyhow::Result<()> {
         Command::Run(RunArguments {
             operator_environment,
             watch_namespace,
-            product_config: _,
             maintenance,
             common,
         }) => {
